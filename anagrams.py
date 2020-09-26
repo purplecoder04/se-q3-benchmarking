6156630e9c8f004b1cba1a227109193738f3b88b
erica best <purple_lover_04@msn.com>
2020-09-26 19:51:59 -0400

    all test past

--- anagrams.py
+++ anagrams.py
@@ -10,7 +10,7 @@ for an arbitrary list of strings.
 
 # Your name here, and any other people/sources who helped.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Erica Best aka purplecoder"
 
 import sys
 
@@ -27,11 +27,11 @@ def find_anagrams(words):
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    anagrams = {}
+    for word in words:
+        anagrams.setdefault(alphabetize(word), [])
+        if word not in anagrams[alphabetize(word)]:
+            anagrams[alphabetize(word)].append(word)
     return anagrams
 
 
